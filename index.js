c61f30d805d7030e716829795d80499b7f03989e
renatoguara2020 <renatoguara2020@gmail.com>
2023-03-09 11:26:44 -0300

    ok

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.get('/usuarios', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello World !')
 })
 
@@ -12,7 +12,14 @@ res.status(200).send("<h3>Rotas no Express na porta 3001</h3><p>Rota '/'")
 
 
 app.post('/usuarios', (req, res)=>{
+ 
+     return res.json({
+      
+         nome: "RENATO ALVES SOARES",
+         email: "renatoguara2020@gmail.com",
+         idade: 51
 
+     });
 
 });
 
