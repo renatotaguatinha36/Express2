d33d2f34089203b570c07be35f2bd2fb2836bdb1
renatoguara2020 <renatoguara2020@gmail.com>
2023-03-09 12:44:21 -0300

    ok

--- sequelize.js
+++ sequelize.js
@@ -2,7 +2,8 @@ var mysql      = require('mysql2');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : ''
+  password : '',
+  database: "mydb"
 });
 
 connection.connect();
@@ -11,5 +12,10 @@ connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
   if (err) throw err;
   console.log('The solution is: ', rows[0].solution);
 });
+var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+  connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
 
 connection.end();
