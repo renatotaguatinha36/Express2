13bb5697ca5ddf178fd33cc7864f327c70e86fcb
renatoguara2020 <renatoguara2020@gmail.com>
2023-03-09 11:06:33 -0300

    ok

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/usuarios', (req, res) => {
+  res.send('Hello World !')
 })
+
+app.get('/', (req, res) =>
+res.status(200).send("<h3>Rotas no Express na porta 3001</h3><p>Rota '/'")
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
