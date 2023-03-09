36a5a9c98c47d439b3224b0380495a060ee90d38
renatoguara2020 <renatoguara2020@gmail.com>
2023-03-09 10:54:56 -0300

    ok

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const  express = require('express');
+const express = require('express')
+const app = express()
+const port = 3001
 
-const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-app.listen(3000, () => 
-	console.log('SERVIDOR INICIADO NA PORTA 3000')
-);
-
-app.get('/usuarios', (req, res) => {
-res.send('SERVIDOR INICIADO NA PORTA 3000');
-
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
